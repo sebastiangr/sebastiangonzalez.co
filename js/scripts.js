90cd344e83eec11d78c0269bb71dca7b3da1bf2a
sebastiangr <sebastiangr@gmail.com>
2015-11-21 03:04:59 -0500

    Added: - Owl carousel for latest entries. - Styles for comments. - Styles for comment-form. - Styles for superior tab. - Fixes...

--- js/scripts.js
+++ js/scripts.js
@@ -90,6 +90,32 @@ var main = ( function($){
     });
 
 
+    var owl = $("#owl-demo");
+
+    owl.owlCarousel({
+        items : 2, //10 items above 1000px browser width
+        itemsDesktop : [1140,2], //5 items between 1000px and 901px
+        itemsDesktopSmall : [992,2], // betweem 900px and 601px
+        itemsTablet: [768,1], //2 items between 600 and 0
+        itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+        pagination : false
+    });
+
+    // Custom Navigation Events
+    $(".next").click(function(){
+        owl.trigger('owl.next');
+    })
+    $(".prev").click(function(){
+        owl.trigger('owl.prev');
+    })
+    $(".play").click(function(){
+        owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
+    })
+    $(".stop").click(function(){
+        owl.trigger('owl.stop');
+    })
+
+
 
     /** Public API */
     return {
