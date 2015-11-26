ff2070ddec9e0146cb237034625c20ac77b1469f
sebastiangr <sebastiangr@gmail.com>
2015-11-26 02:41:23 -0500

    Added: - Finished desktop menu: search box implemented, some fixes on scroll (better JS), css update. - Implemented <sup>, styles and scrolling animation. - Added icons: Quotation, arrows. - Smooth scroll script only on Chrome. - TODO: Gototop?, Lightbox for images, Menu mobile!!!

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,18 @@
 var main = ( function($){
 
-    var $window             = $(window),
-        $homeBanner         = $('#home-banner'),         /** Home Banner */
-        $homeBannerH2       = $homeBanner.find('h2'),
-        $bannerText         = $('.header-content');
+    var $window = $(window),
+        $body = $('html, body'),
+        $homeBanner = $("#home-banner"),   /** Home Banner */
+        $homeBannerH2 = $homeBanner.find("h2"), /** Home Banner Title H2 */
+        $bannerText = $(".header-content"),
+        $header = $("header"),
+        $headerLogo = $("header .logo img"),
+        $headerInner = $(".header-in"),
+        owl = $("#entries-slider"),
+        $btnNext = $(".next"),
+        $btnPrev = $(".prev");
 
-    var settings            = {
-            verbose: false
-        },
-        tapTargets, scrollPos, oneOff;
-
+    var settings = { verbose: false }, tapTargets, scrollPos, oneOff;
 
     var init = function( options ){
 
@@ -19,15 +22,35 @@ var main = ( function($){
         tapTargets = [];
 
         if( settings.verbose )
-            console.log( 'Verbose mode on.' );
+            console.log( "Verbose mode on." );
 
         /** Banner Parallax */
         $window.scroll(function () {
             scrollBanner();
+            scrollMenu();
+        });
+
+
+        // SCROLL TO ANCHOR
+        $("a[href*=#]").click(function(e) {
+            e.preventDefault();
+            var currentAnchor = $(this);
+            var currentClass = $(this).attr('class');
+
+            // Links for <sup> elements
+            if (currentClass == "suplink") {
+                $body.animate({
+                    scrollTop: $(currentAnchor.attr('href')).offset().top - 140
+                }, 1200);
+            } else {
+                $body.animate({
+                    scrollTop: $(currentAnchor.attr('href')).offset().top - 80
+                }, 1200);
+            }
         });
 
         if( settings.verbose)
-            console.log( 'All of init fired.' );
+            console.log( "All of init fired." );
 
     };
 
@@ -45,76 +68,56 @@ var main = ( function($){
         scrollPos = $(this).scrollTop();
 
         if( settings.verbose)
-            console.log( 'Scroll top: ' + scrollPos );
+            console.log( "Scroll top: " + scrollPos );
 
         /** Scroll and fade out the banner text */
         $bannerText.css({
-            'margin-top' : ( scrollPos / 3 ) + "px", // Define el movimiento leve hacia abajo al hacer scroll
-            'opacity' : 1 - ( scrollPos / 400 ), // En qué posición Y se empieza a desaparecer el div
-            '-ms-filter' : 'progid:DXImageTransform.Microsoft.Alpha(Opacity=' + 1 - ( scrollPos / 400 ) + ')'
+            "margin-top" : ( scrollPos / 3 ) + "px", // Define el movimiento leve hacia abajo al hacer scroll
+            "opacity" : 1 - ( scrollPos / 400 ), // En qué posición Y se empieza a desaparecer el div
+            "-ms-filter" : "progid:DXImageTransform.Microsoft.Alpha(Opacity=" + 1 - ( scrollPos / 400 ) + ")"
         });
 
         /** Scroll the background of the banner */
         $homeBanner.css({
-            'background-position' : 'center ' + (- scrollPos / 2 ) + "px"
+            "background-position" : "center " + (- scrollPos / 2 ) + "px"
         });
     };
 
-
-    $(window).scroll(function(){
+    /**
+     * Scroll Menu Function
+     */
+    var scrollMenu = function() {
         if ($(this).scrollTop() > 450) {
-
-            $("header").addClass("black-bg affix");
-            $("header .logo img").addClass("header-logo-scroll");
-            $(".header-in").addClass("header-fixed");
-
-            // $("header").addClass("black-bg affix").delay(600).queue(function(next){
-            //     $('header .logo img').addClass('header-logo-scroll');
-            //     $('.header-in').addClass('header-fixed');
-            //         // next();
-            // });
-
+            $header.addClass("black-bg affix");
+            $headerLogo.addClass("header-logo-scroll");
+            $headerInner.addClass("header-fixed");
         } else {
-
-            $("header").removeClass("black-bg affix");
-            $("header .logo img").removeClass("header-logo-scroll");
-            $(".header-in").removeClass("header-fixed");
-
-
-            // $("header").removeClass("black-bg affix").delay(100).queue(function(next){
-            //     $('header .logo img').removeClass('header-logo-scroll');
-            //     $('.header-in').removeClass('header-fixed');
-            // });
+            $header.removeClass("black-bg affix");
+            $headerLogo.removeClass("header-logo-scroll");
+            $headerInner.removeClass("header-fixed");
         }
+    };
 
-    });
-
-
-    var owl = $("#owl-demo");
-
+    /**
+     * OWL Carousel
+     */
     owl.owlCarousel({
         items : 2, //10 items above 1000px browser width
         itemsDesktop : [1140,2], //5 items between 1000px and 901px
         itemsDesktopSmall : [992,2], // betweem 900px and 601px
         itemsTablet: [768,1], //2 items between 600 and 0
         itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
-        pagination : false
+        pagination : true,
+        autoPlay: true
     });
 
     // Custom Navigation Events
-    $(".next").click(function(){
-        owl.trigger('owl.next');
+    $btnNext.click(function(){
+        owl.trigger("owl.next");
     })
-    $(".prev").click(function(){
-        owl.trigger('owl.prev');
+    $btnPrev.click(function(){
+        owl.trigger("owl.prev");
     })
-    $(".play").click(function(){
-        owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
-    })
-    $(".stop").click(function(){
-        owl.trigger('owl.stop');
-    })
-
 
 
     /** Public API */
---
+++ js/scripts2.js
@@ -0,0 +1,154 @@
+// var main = ( function($){
+
+//     var $window = $(window),
+//         $body = $('body'),
+//         $homeBanner = $("#home-banner"),   /** Home Banner */
+//         $homeBannerH2 = $homeBanner.find("h2"), /** Home Banner Title H2 */
+//         $bannerText = $(".header-content"),
+//         $header = $("header"),
+//         $headerLogo = $("header .logo img"),
+//         $headerInner = $(".header-in"),
+//         owl = $("#entries-slider"),
+//         $btnNext = $(".next"),
+//         $btnPrev = $(".prev");
+
+    // var settings = { verbose: false }, tapTargets, scrollPos, oneOff;
+
+    // var init = function( options ){
+
+    //     $.extend( settings, options );
+
+    //     oneOff = true;
+    //     tapTargets = [];
+
+    //     if( settings.verbose )
+    //         console.log( "Verbose mode on." );
+
+    //     /** Banner Parallax */
+    //     $window.scroll(function () {
+    //         scrollBanner();
+    //         // scrollMenu();
+    //     });
+
+
+
+
+
+    //     if( settings.verbose)
+    //         console.log( "All of init fired." );
+
+    // };
+
+    // /**
+    //  * Parallax Banner Function
+    //  */
+    // var scrollBanner = function() {
+
+    //     if( settings.verbose && oneOff ) {
+    //         console.log('scrollBanner');
+    //         oneOff = false;
+    //     }
+
+    //     /** Get the scroll position of the page */
+    //     scrollPos = $(this).scrollTop();
+
+    //     if( settings.verbose)
+    //         console.log( "Scroll top: " + scrollPos );
+
+    //     /** Scroll and fade out the banner text */
+    //     $bannerText.css({
+    //         "margin-top" : ( scrollPos / 3 ) + "px", // Define el movimiento leve hacia abajo al hacer scroll
+    //         "opacity" : 1 - ( scrollPos / 400 ), // En qué posición Y se empieza a desaparecer el div
+    //         "-ms-filter" : "progid:DXImageTransform.Microsoft.Alpha(Opacity=" + 1 - ( scrollPos / 400 ) + ")"
+    //     });
+
+    //     /** Scroll the background of the banner */
+    //     $homeBanner.css({
+    //         "background-position" : "center " + (- scrollPos / 2 ) + "px"
+    //     });
+    // };
+
+
+
+
+
+
+
+
+
+
+jQuery(document).ready( function() {
+
+
+
+    var $window = $(window),
+        $body = $('body'),
+        $homeBanner = $("#home-banner"),   /** Home Banner */
+        $homeBannerH2 = $homeBanner.find("h2"), /** Home Banner Title H2 */
+        $bannerText = $(".header-content"),
+        $header = $("header"),
+        $headerLogo = $("header .logo img"),
+        $headerInner = $(".header-in"),
+        owl = $("#entries-slider"),
+        $btnNext = $(".next"),
+        $btnPrev = $(".prev");
+
+
+       // SCROLL TO ANCHOR
+        $("a[href*=#]").click(function(e) {
+            e.preventDefault();
+            var currentAnchor = $(this);
+            var currentClass = $(this).attr('class');
+
+            // Links for <sup> elements
+            if (currentClass == "suplink") {
+                $('html, body').animate({
+                    scrollTop: $(currentAnchor.attr('href')).offset().top - 140
+                }, 1200);
+            }  else if (currentClass == "another") {
+                $('html, body').animate({
+                    scrollTop: $(currentAnchor.attr('href')).offset().top - 80
+                }, 1200);
+            }
+        });
+
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 450) {
+
+            $("header").addClass("black-bg affix");
+            $("header .logo img").addClass("header-logo-scroll");
+            $(".header-in").addClass("header-fixed");
+
+        } else {
+
+            $("header").removeClass("black-bg affix");
+            $("header .logo img").removeClass("header-logo-scroll");
+            $(".header-in").removeClass("header-fixed");
+        }
+
+    });
+
+    /**
+     * OWL Carousel
+     */
+    owl.owlCarousel({
+        items : 2, //10 items above 1000px browser width
+        itemsDesktop : [1140,2], //5 items between 1000px and 901px
+        itemsDesktopSmall : [992,2], // betweem 900px and 601px
+        itemsTablet: [768,1], //2 items between 600 and 0
+        itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+        pagination : true,
+        autoPlay: true
+    });
+
+    // Custom Navigation Events
+    $btnNext.click(function(){
+        owl.trigger("owl.next");
+    })
+    $btnPrev.click(function(){
+        owl.trigger("owl.prev");
+    })
+
+
+});
